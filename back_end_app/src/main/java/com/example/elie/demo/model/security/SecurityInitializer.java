73fd887dcdca9deeedb61a0ae6fa053b627471b5
Elie nsampi <elientumba@live.fr>
2018-05-31 11:13:37 +0800

    lots of refactoring done

--- back_end_app/src/main/java/com/example/elie/demo/model/security/SecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.example.elie.demo.model.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-
-}
--- back_end_app/src/main/java/com/example/elie/demo/repository/RoleDao.java
+++
@@ -1,6 +0,0 @@
-package com.example.elie.demo.repository;
-import com.example.elie.demo.model.security.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleDao extends CrudRepository<Role, Long> {
-}
